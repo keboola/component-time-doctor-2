45bed1c8608bc1ff3f23a64c57a9d72b10960d62
SgtMarmite <d.novotny31415@gmail.com>
2022-11-25 15:42:53 +0100

    gui updates

--- src/component.py
+++ src/component.py
@@ -24,6 +24,7 @@ KEY_RELATIVE_RANGE = 'relative_range'
 # list of mandatory parameters => if some is missing,
 # component will fail with readable message on initialization.
 REQUIRED_PARAMETERS = [KEY_EMAIL, KEY_PASSWORD, KEY_COMPANY_ID, KEY_ENDPOINTS, KEY_USE_RELATIVE_RANGE]
+ENDPOINTS = list(ENDPOINT_MAPPING.keys())
 
 
 class Component(ComponentBase):
@@ -44,31 +45,32 @@ class Component(ComponentBase):
         self.email = params.get(KEY_EMAIL)
         self.password = params.get(KEY_PASSWORD)
         self.company_id = params.get(KEY_COMPANY_ID)
-        self._from = params.get(KEY_FROM)
-        self._to = params.get(KEY_TO)
         self.now = datetime.now()
         self.use_relative_range = params.get(KEY_USE_RELATIVE_RANGE)
         self.relative_range = params.get(KEY_RELATIVE_RANGE)
-        endpoints = params.get(KEY_ENDPOINTS)
-        endpoints.sort()
-        self.endpoints = endpoints
+        # endpoints = params.get(KEY_ENDPOINTS)
+        # endpoints.sort()
+        # self.endpoints = endpoints
         self.dt_format = "%Y-%m-%dT%H:%M:%S"
+        _from = params.get(KEY_FROM)
+        _to = params.get(KEY_TO)
+        self._from, self._to = self.make_ts_from_ts_string(_from, _to)
+
+        endpoints = params.get("endpoints")
+        self.endpoints = []
+        for endpoint in endpoints:
+            if endpoint in ENDPOINTS:
+                if endpoints.get(endpoint):
+                    self.endpoints.append(endpoint)
+            else:
+                raise UserException(f"Endpoint {endpoint} is not supported.")
+        logging.info(f"Component will process following endpoints: {self.endpoints}")
 
     def run(self):
         """
         Main execution code
         """
-        if self.use_relative_range:
-            self._from = self.get_relative_date()
-            self._to = self.now.strftime(self.dt_format)
-            logging.info(f"Component will use relative date as 'from' parameter: {self._from} and will fetch data "
-                         f"until {self._to}")
-        else:
-            if not self._from:
-                self._from = self.now.strftime(self.dt_format)
-            if not self._to:
-                self._to = self.now.strftime(self.dt_format)
-            logging.info(f"Component will use custom time window to fetch data from {self._from} to {self._to}")
+        logging.info(f"Component will use custom time window to fetch data from {self._from} to {self._to}")
 
         try:
             client = TimeDoctor2Client(email=self.email, password=self.password, company_id=self.company_id,
@@ -84,6 +86,13 @@ class Component(ComponentBase):
             self.write_manifest(table)
             if endpoint == '_users':
                 client.get_list_of_users(table.full_path)
+
+    def make_ts_from_ts_string(self, _from, _to):
+        _from = _from if _from is not "" else "now"
+        _to = _to if _to is not "" else "now"
+        print(_from, _to)
+        start_date, end_date = parse_datetime_interval(_from, _to, self.dt_format)
+        return start_date, end_date
 
     def get_relative_date(self):
         start_date, end_date = parse_datetime_interval(self.relative_range,
